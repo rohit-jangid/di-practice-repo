81472561aaae6391bdd40b87f32483b4bd38ac7c
Rohit Jangid <rohit.jangid@internshala.com>
2022-01-10 15:21:59 +0530

    try view binding

--- app/src/main/java/com/rohfl/di/MainActivity.java
+++ app/src/main/java/com/rohfl/di/MainActivity.java
@@ -3,6 +3,9 @@ package com.rohfl.di;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.rohfl.di.databinding.ActivityMainBinding;
 
 import javax.inject.Inject;
 
@@ -15,10 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Inject OtherObject otherObject;
 
+    ActivityMainBinding activityMainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        View v = activityMainBinding.getRoot();
+
+        setContentView(v);
 
 //        sampleObject.hello();
 
